bf0074b2982b947aaa86cd604895b59aed67d385
Ahmed Hussein Morgan <39451996+ahmedmorgan210@users.noreply.github.com>
2024-03-27 14:45:48 +0200

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -9,17 +9,18 @@
  * struct binary_tree_s - Binary tree node
  *
  * @n: Integer stored in the node
- * @parent: a Pointer to the parent node
- * @left: a Pointer to the left child node
- * @right: a Pointer to the right child node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
@@ -67,4 +68,5 @@ heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
-#endif
+
+#endif /* _BINARY_TREES_H_ */
